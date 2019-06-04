424815142682fb1e43ba2d0bf845db5967b1f9ff
Blake Edwards <blake8090@gmail.com>
2019-06-03 20:26:03 -0400

    Initial commit

---
+++ src/main/kotlin/com/blake8090/circuitbreak/CircuitBreak.kt
@@ -0,0 +1,8 @@
+package com.blake8090.circuitbreak
+
+import com.badlogic.gdx.Game
+
+class CircuitBreak : Game() {
+    override fun create() {
+    }
+}
---
+++ src/main/kotlin/com/blake8090/circuitbreak/Main.kt
@@ -0,0 +1,8 @@
+package com.blake8090.circuitbreak
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+
+fun main() {
+    LwjglApplication(CircuitBreak(), LwjglApplicationConfiguration())
+}
