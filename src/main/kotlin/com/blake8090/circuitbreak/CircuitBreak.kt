88a314f0a3d5c9766f07a341c2c93b2a873839df
Blake Edwards <blake8090@gmail.com>
2019-06-06 18:43:55 -0400

    Add logger

--- src/main/kotlin/com/blake8090/circuitbreak/CircuitBreak.kt
+++ src/main/kotlin/com/blake8090/circuitbreak/CircuitBreak.kt
@@ -1,8 +1,38 @@
 package com.blake8090.circuitbreak
 
 import com.badlogic.gdx.Game
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import org.pmw.tinylog.Configurator
+import org.pmw.tinylog.Level
+import org.pmw.tinylog.Logger
+import org.pmw.tinylog.writers.ConsoleWriter
+import org.pmw.tinylog.writers.FileWriter
+
+private const val LOG_FORMAT =
+    "{date:yyyy-MM-dd HH:mm:ss} [{thread}] {class_name}.{method}() Line:{line} {level}: {message}"
 
 class CircuitBreak : Game() {
+    private lateinit var context: GameContext
+
     override fun create() {
+        setupLogger()
+        context = GameContext()
     }
+
+    private fun setupLogger() {
+        Configurator.defaultConfig()
+            .writer(FileWriter("log.txt"))
+            .addWriter(ConsoleWriter())
+            .formatPattern(LOG_FORMAT)
+            .level(Level.TRACE)
+            .activate()
+        Logger.info("Initialized logger")
+    }
+}
+
+/**
+ * Used for injecting game-wide dependencies into other classes
+ */
+class GameContext {
+    val batch = SpriteBatch()
 }
