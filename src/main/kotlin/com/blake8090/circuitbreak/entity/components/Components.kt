9b176031f430f136c33e81f20a2148fbe99ae7a8
Blake Edwards <blake8090@gmail.com>
2019-06-16 11:57:29 -0400

    Serialization for entity components

--- src/main/kotlin/com/blake8090/circuitbreak/entity/components/Components.kt
+++ src/main/kotlin/com/blake8090/circuitbreak/entity/components/Components.kt
@@ -1,5 +1,17 @@
 package com.blake8090.circuitbreak.entity.components
 
+import com.fasterxml.jackson.annotation.JsonSubTypes
+import com.fasterxml.jackson.annotation.JsonTypeInfo
+
+@JsonTypeInfo(
+    use = JsonTypeInfo.Id.NAME,
+    include = JsonTypeInfo.As.PROPERTY,
+    property = "type"
+)
+@JsonSubTypes(
+    JsonSubTypes.Type(value = PositionComponent::class),
+    JsonSubTypes.Type(value = TextureComponent::class)
+)
 open class Component
 
 data class PositionComponent(
